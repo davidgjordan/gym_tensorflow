89995f8d8fe2284800a10fc2951812424605e1f9
davidgjordan <davidgjordancbba@gmail.com>
2018-01-20 18:44:40 -0400

    converter matriz

--- agent_dos/agent_dos.py
+++ agent_dos/agent_dos.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import gym
 # La imagenes tienen dimension de 28x28
-#mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # GYM
 
@@ -27,102 +27,89 @@ for i_episode in range(10):
         aux_action.append(action)
         aux_obs.append(observation)
 
-        #print("Action: ", action)
-        #print("Reward: ", reward)
+        # print("Action: ", action)
+        # print("Reward: ", reward)
 
     if aux_reward > 250.0:
         list_obs.append(aux_obs)
         list_action_esperadas.append(aux_action)
         print("este juego paso: ", i_episode)
 
-    #print("juego: ", i_episode)
-    #print("Total Reward por el juego: ", aux_reward)
+    # print("juego: ", i_episode)
+    # print("Total Reward por el juego: ", aux_reward)
     aux_reward = 0
 
 for action in list_action_esperadas:
-    #print("action: ", action)
+    # print("action: ", action)
     pass
     # LISTAS CARGADAS CON OBSEVATIONS Y ACCIONES ESPERADAS AHORA NORMALIZAR LAS SALIDAS ESPERADAS
 
 list_normalize_actions, aux = [], []
-for action_elegida_vec in list_action_esperadas:
-    for act in action_elegida_vec:
-        aux = np.zeros(((tam_teclas_disponibles + 1), 1))
-        #aux = np.linspace(0, 0.01, (tam_teclas_disponibles + 1))
+for vec_action_por_juego in list_action_esperadas:
+    vec_aux = []
+    for act in vec_action_por_juego:
+        aux = np.zeros(((tam_teclas_disponibles)))
+        # aux = np.linspace(0, 0.01, (tam_teclas_disponibles + 1))
         aux[act] = 1
-        list_normalize_actions.append(aux)
+        vec_aux.append(aux)
+
+    list_normalize_actions.append(vec_aux)
 
 for vec_action in list_normalize_actions:
-    print("vec_action : ")
+    #print("vec_action : ", vec_action)
     for data in vec_action:
-        print(data)
-
-# x_input_observatios=tf.placeholder(tf.float32,[None,784]) #imagen del numero descompuesta a un vector
-# Pesos=tf.Variable(tf.zeros([784,10])) #Matriz de pesos, 784 para recibir la observ, 10 por las posible salidas de teclas
-#                                     #seria conveniete normalizae las teclas en [0,0,1,0,0,0,0,0] proporcionaron la tecla 2 etc
-# bias=tf.Variable(tf.zeros([10])) #Vector con bias
-# y_oper_nodos_entradas=tf.matmul(x_input_observatios,Pesos)+bias #La operacion que se hara en los nodos que reciben entradas
-# yR_salidas_aciones_esperadas=tf.placeholder(tf.float32,[None,10]) # Matriz con las acciones esperadas de nuestro set de datos
-
-# #Definir la funcion de costo entropia cruzada (Cross Entropy) para poder medir el error. La salida sera con Softmax
-# fun_softmax_medir_error=tf.nn.softmax_cross_entropy_with_logits(labels=yR_salidas_aciones_esperadas,logits=y_oper_nodos_entradas)
-# costo=tf.reduce_mean(fun_softmax_medir_error)
-# optimizador=tf.train.GradientDescentOptimizer(0.5).minimize(costo)
-
-# #Correr la grafica computacional
-# prediccion = tf.equal(tf.argmax(y_oper_nodos_entradas, 1), tf.argmax(yR_salidas_aciones_esperadas, 1)) #Nos da arreglo de booleanos para decirnos
-#                                                          #cuales estan bien y_entradas cuales no
-# exactitud_predicciones = tf.reduce_mean(tf.cast(prediccion, tf.float32))#Nos da el porcentaje sobre el arreglo de prediccion
-
-# #Devuelve el indice con el valor mas grande en los ejes de un tensor. (argumentos en desuso)
-# Produccion = tf.argmax(y_oper_nodos_entradas,1) # recordando que estamos trabajando sobre una matriz de acciones del tipo [0,0,1,0,0,0,0,0] proporcionaron la tecla 2 etc 1 es el mas alto
-# init=tf.global_variables_initializer()
+        # print(data)
+        pass
 
 
-# #Entrenar algoritmo
-# #Funcion que usaremos para ver que tan bien va a aprendiendo nuestro modelo
-# def avance(epoca_i, sess, last_features, last_labels):
-#     costoActual = sess.run(costo,feed_dict={x_input_observatios: last_features, yR_salidas_aciones_esperadas: last_labels})
-#     Certeza = sess.run(exactitud_predicciones,feed_dict={x_input_observatios:mnist.validation.images,yR_salidas_aciones_esperadas: mnist.validation.labels})
-#     print('Epoca: {:<4} - Costo: {:<8.3} Certeza: {:<5.3}'.format(epoca_i,costoActual,Certeza))
+# RED NEURONAL   REVISAR LAS ENTRADAS Y  VOLVER LOS OBSERVATIONS Y ACTIONS A MATRIZES COMO EN EL AGENT UNO
+# imagen del numero descompuesta a un vector
+x_input_observations = tf.placeholder(tf.float32, [None, 1])
+# Matriz de pesos, 784 para recibir la observ, 10 por las posible salidas de teclas
+Pesos = tf.Variable(tf.zeros([1, 1]))
+                                    # seria conveniete normalizae las teclas en [0,0,1,0,0,0,0,0] proporcionaron la tecla 2 etc
+bias = tf.Variable(tf.zeros([1]))  # Vector con bias
+# La operacion que se hara en los nodos que reciben entradas
+y_oper_nodos_entradas = tf.matmul(x_input_observations, Pesos) + bias
+# Matriz con las acciones esperadas de nuestro set de datos
+yR_salidas_aciones_esperadas = tf.placeholder(tf.float32, [None, 1])
+# Definir la funcion de costo entropia cruzada (Cross Entropy) para poder medir el error. La salida sera con Softmax
+fun_softmax_medir_error = tf.nn.softmax_cross_entropy_with_logits(
+    labels=yR_salidas_aciones_esperadas, logits=y_oper_nodos_entradas)
+costo = tf.reduce_mean(fun_softmax_medir_error)
+optimizador = tf.train.GradientDescentOptimizer(0.5).minimize(costo)
+# Correr la grafica computacional
+prediccion = tf.equal(tf.argmax(y_oper_nodos_entradas, 1), tf.argmax(
+    yR_salidas_aciones_esperadas, 1))  # Nos da arreglo de booleanos para decirnos
+                                                         # cuales estan bien y_entradas cuales no
+# Nos da el porcentaje sobre el arreglo de prediccion
+exactitud_predicciones = tf.reduce_mean(tf.cast(prediccion, tf.float32))
+# Devuelve el indice con el valor mas grande en los ejes de un tensor. (argumentos en desuso)
+# recordando que estamos trabajando sobre una matriz de acciones del tipo [0,0,1,0,0,0,0,0] proporcionaron la tecla 2 etc 1 es el mas alto
+Produccion = tf.argmax(y_oper_nodos_entradas, 1)
+init = tf.global_variables_initializer()
+ # Entrenar algoritmo
+# Funcion que usaremos para ver que tan bien va a aprendiendo nuestro modelo
 
 
-# with tf.Session() as sess:
-#     sess.run(init)
-#     for epoca_i in range(1000):
-#         lotex, lotey = mnist.train.next_batch(100)# lotex = observaciones   lotey = acciones esperadas
-#         sess.run(optimizador, feed_dict={x_input_observatios: lotex, yR_salidas_aciones_esperadas: lotey})
-#         if (epoca_i%50==0):
-#             avance(epoca_i, sess, lotex, lotey)
-#     print('RESULTADO FINAL: ',sess.run(exactitud_predicciones, feed_dict={x_input_observatios: mnist.test.images,yR_salidas_aciones_esperadas: mnist.test.labels}))
-#     print ('Resultado de una imagen',sess.run(Produccion,feed_dict={x_input_observatios: mnist.test.images[5].reshape(1,784)}))
-#     print("Produccion: " , sess.run(Produccion) )
+def avance(epoca_i, sess, last_features, last_labels):
+    costoActual = sess.run(costo, feed_dict={
+                           x_input_observations: last_features, yR_salidas_aciones_esperadas: last_labels})
+    Certeza = sess.run(exactitud_predicciones, feed_dict={
+                       x_input_observations: list_obs, yR_salidas_aciones_esperadas: list_normalize_actions})
+    print(
+        'Epoca: {:<4} - Costo: {:<8.3} Certeza: {:<5.3}'.format(epoca_i, costoActual, Certeza))
 
-
-''' Epoca: 0    - Costo: 1.8      Certeza: 0.111
-Epoca: 50   - Costo: 0.382    Certeza: 0.875
-Epoca: 100  - Costo: 0.407    Certeza: 0.898
-Epoca: 150  - Costo: 0.316    Certeza: 0.896
-Epoca: 200  - Costo: 0.34     Certeza: 0.908
-Epoca: 250  - Costo: 0.313    Certeza: 0.912
-Epoca: 300  - Costo: 0.286    Certeza: 0.907
-Epoca: 350  - Costo: 0.391    Certeza: 0.914
-Epoca: 400  - Costo: 0.509    Certeza: 0.909
-Epoca: 450  - Costo: 0.478    Certeza: 0.914
-Epoca: 500  - Costo: 0.506    Certeza: 0.914
-Epoca: 550  - Costo: 0.215    Certeza: 0.917
-Epoca: 600  - Costo: 0.273    Certeza: 0.914
-Epoca: 650  - Costo: 0.227    Certeza: 0.917
-Epoca: 700  - Costo: 0.224    Certeza: 0.919
-Epoca: 750  - Costo: 0.177    Certeza: 0.922
-Epoca: 800  - Costo: 0.324    Certeza: 0.919
-Epoca: 850  - Costo: 0.248    Certeza: 0.924
-Epoca: 900  - Costo: 0.258    Certeza: 0.923
-Epoca: 950  - Costo: 0.162    Certeza: 0.918
-RESULTADO FINAL:  0.92
-Resultado de una imagen [1]
-In [8]:
-mnist.test.labels[5]
-Out[8]:
-array([ 0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.])
-In [ ]: '''
+with tf.Session() as sess:
+    sess.run(init)
+    for epoca_i in range(1):
+        #lotex, lotey = mnist.train.next_batch(100)# lotex = observaciones   lotey = acciones esperadas
+        lotex = list_obs
+        lotey = list_normalize_actions
+        #print("lotex: ", lotex)
+        print("lotey: ", lotey)
+        sess.run(optimizador, feed_dict={x_input_observations: lotex, yR_salidas_aciones_esperadas: lotey})
+        if (epoca_i%50==0):pass
+            #avance(epoca_i, sess, lotex, lotey)
+    #print('RESULTADO FINAL: ',sess.run(exactitud_predicciones, feed_dict={x_input_observations: mnist.test.images,yR_salidas_aciones_esperadas: mnist.test.labels}))
+    #print ('Resultado de una imagen',sess.run(Produccion,feed_dict={x_input_observations: mnist.test.images[5].reshape(1,784)}))
--- agent_dos/agent_uno.py
+++ agent_dos/agent_uno.py
@@ -1,47 +1,65 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist=input_data.read_data_sets("MNIST_data/", one_hot=True) #La imagenes tienen dimension de 28x28
+# La imagenes tienen dimension de 28x28
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
-#none puede tomar el tamano de observciones q ingresemos
-x=tf.placeholder(tf.float32,[None,784]) #imagen del numero descompuesta a un vector
-P=tf.Variable(tf.zeros([784,10])) #Matriz de pesos, 784 para recibir la imagen, 10 por las posible salidas
-b=tf.Variable(tf.zeros([10])) #Vector con bias
-y=tf.matmul(x,P)+b #La operacion que se hara en los nodos que reciben entradas
-yR=tf.placeholder(tf.float32,[None,10]) # Matriz con las etiquetas REALES del set de datos
+# none puede tomar el tamano de observciones q ingresemos
+# imagen del numero descompuesta a un vector
+x = tf.placeholder(tf.float32, [None, 784])
+# Matriz de pesos, 784 para recibir la imagen, 10 por las posible salidas
+P = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))  # Vector con bias
+# La operacion que se hara en los nodos que reciben entradas
+y = tf.matmul(x, P) + b
+# Matriz con las etiquetas REALES del set de datos
+yR = tf.placeholder(tf.float32, [None, 10])
 
-#Definir la funcion de costo entropia cruzada (Cross Entropy) para poder medir el error. La salida sera con Softmax
-softmax=tf.nn.softmax_cross_entropy_with_logits(labels=yR,logits=y)
-costo=tf.reduce_mean(softmax)
-optimizador=tf.train.GradientDescentOptimizer(0.5).minimize(costo)
+# Definir la funcion de costo entropia cruzada (Cross Entropy) para poder medir el error. La salida sera con Softmax
+softmax = tf.nn.softmax_cross_entropy_with_logits(labels=yR, logits=y)
+costo = tf.reduce_mean(softmax)
+optimizador = tf.train.GradientDescentOptimizer(0.5).minimize(costo)
 
-#Correr la grafica computacional
-prediccion = tf.equal(tf.argmax(y, 1), tf.argmax(yR, 1)) #Nos da arreglo de booleanos para decirnos 
-                                                         #cuales estan bien y cuales no
-accuracy = tf.reduce_mean(tf.cast(prediccion, tf.float32))#Nos da el porcentaje sobre el arreglo de prediccion
-Produccion = tf.argmax(y,1)
-init=tf.global_variables_initializer()
+# Correr la grafica computacional
+# Nos da arreglo de booleanos para decirnos
+prediccion = tf.equal(tf.argmax(y, 1), tf.argmax(yR, 1))
+# cuales estan bien y cuales no
+# Nos da el porcentaje sobre el arreglo de prediccion
+accuracy = tf.reduce_mean(tf.cast(prediccion, tf.float32))
+Produccion = tf.argmax(y, 1)
+init = tf.global_variables_initializer()
 
 
-#Entrenar algoritmo
-#Funcion que usaremos para ver que tan bien va a aprendiendo nuestro modelo 
+# Entrenar algoritmo
+# Funcion que usaremos para ver que tan bien va a aprendiendo nuestro modelo
 def avance(epoca_i, sess, last_features, last_labels):
-    costoActual = sess.run(costo,feed_dict={x: last_features, yR: last_labels})
-    Certeza = sess.run(accuracy,feed_dict={x:mnist.validation.images,yR: mnist.validation.labels})
-    print('Epoca: {:<4} - Costo: {:<8.3} Certeza: {:<5.3}'.format(epoca_i,costoActual,Certeza))
-
+    costoActual = sess.run(
+        costo, feed_dict={x: last_features, yR: last_labels})
+    Certeza = sess.run(accuracy, feed_dict={
+                       x: mnist.validation.images, yR: mnist.validation.labels})
+    print(
+        'Epoca: {:<4} - Costo: {:<8.3} Certeza: {:<5.3}'.format(epoca_i, costoActual, Certeza))
 
 
 with tf.Session() as sess:
     sess.run(init)
     for epoca_i in range(1000):
-        lotex, lotey = mnist.train.next_batch(100)
+        lotex, lotey = mnist.train.next_batch(3)
+        print ("epoca_i:", epoca_i)
+        #print ("lotex:", lotex)
+        #print ("lotey:", lotey)
+        # for data in lotex:
+        #   print ("data XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX: ", data)
+        # for data in lotey:
+        print ("data YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY: ", lotey)
+
         sess.run(optimizador, feed_dict={x: lotex, yR: lotey})
-        if (epoca_i%50==0):
-            avance(epoca_i, sess, lotex, lotey)
-    print('RESULTADO FINAL: ',sess.run(accuracy, feed_dict={x: mnist.test.images,yR: mnist.test.labels}))
-    print ('Resultado de una imagen',sess.run(Produccion,feed_dict={x: mnist.test.images[5].reshape(1,784)}))
+        if (epoca_i % 50 == 0):
+            pass
+            #avance(epoca_i, sess, lotex, lotey)
+           #print('RESULTADO FINAL: ',sess.run(accuracy, feed_dict={x: mnist.test.images,yR: mnist.test.labels}))
+           #print ('Resultado de una imagen',sess.run(Produccion,feed_dict={x: mnist.test.images[5].reshape(1,784)}))
 ''' Epoca: 0    - Costo: 1.8      Certeza: 0.111
 Epoca: 50   - Costo: 0.382    Certeza: 0.875
 Epoca: 100  - Costo: 0.407    Certeza: 0.898
